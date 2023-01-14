fe0c1e3ec38f2af244135411b8f62a0ce97396d7
bohdan <57547705+r0ugeEngine@users.noreply.github.com>
2023-01-14 17:58:16 +0200

    change: Changed smt

--- contracts/AragonDAO.sol
+++ contracts/AragonDAO.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.16;
 
 import "@openzeppelin/contracts/governance/Governor.sol";
-import "./GovernorCountingSimpleUpdated.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+
+import "./GovernorCountingSimpleUpdated.sol";
 
 contract AragonDAO is
     Governor,
